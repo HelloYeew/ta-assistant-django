13a2fc8a05036458449fec7fb42524f8600bfa13
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-06 19:15:22 +0700

    Make username prohibit from edit and student id on profile can change

--- users/forms.py
+++ users/forms.py
@@ -8,11 +8,10 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField()
     last_name = forms.CharField()
-    student_id = forms.IntegerField()
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'student_id', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -20,10 +19,12 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['email']
 
 
 class ProfileUpdateForm(forms.ModelForm):
+    student_id = forms.IntegerField()
+
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['student_id', 'image']

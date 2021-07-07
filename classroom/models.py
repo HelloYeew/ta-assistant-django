da3eb337be6f5f5fc25b4bbb48f7cde67f78cbc7
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-08 04:04:14 +0700

    Change default value on class database

--- classroom/models.py
+++ classroom/models.py
@@ -7,9 +7,9 @@ class Class(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default_classroom.jpeg', upload_to='class_pics')
-    student = models.CharField(max_length=100000)
-    ta = models.CharField(max_length=100000)
-    teacher = models.CharField(max_length=100000)
+    student = models.CharField(max_length=100000, default=0)
+    ta = models.CharField(max_length=100000, default=0)
+    teacher = models.CharField(max_length=100000, default=0)
 
     def __str__(self):
         return self.name

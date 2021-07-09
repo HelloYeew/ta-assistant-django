6d7e0f184748ae132802127ef73469e949d804c4
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-10 03:14:33 +0700

    Fix error when student value is 0

--- database_function/add_remove_member.py
+++ database_function/add_remove_member.py
@@ -6,11 +6,18 @@ from database_function.conversion import convert_member, get_all_class_member, r
 
 def add_student_to_db(id_to_add, class_id):
     target_class = Class.objects.get(id=class_id)
-    clean_list = remove_duplicate(f"{target_class.student},{id_to_add}", "str")
-    checked_user = get_all_class_member(convert_member(clean_list), check=True)
-    if checked_user:
-        target_class.student = target_class.student + "," + id_to_add
+    if target_class.student == "0":
+        target_class.student = id_to_add
         target_class.save()
         return True
     else:
-        return False
+        clean_list = remove_duplicate(f"{target_class.student},{id_to_add}", "str")
+        checked_user = get_all_class_member(convert_member(clean_list), check=True)
+        if checked_user:
+            target_class.student = clean_list
+            target_class.save()
+            return True
+        else:
+            return False
+
+# TODO: Implement this function to TA and teacher (Duplication must check on all TA and teacher too.)

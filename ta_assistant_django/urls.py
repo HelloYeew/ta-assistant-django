3ed557a8e3ba9ca0d97605cdd7d590d4328e7d11
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-11 02:12:00 +0700

    Remove unused function

--- ta_assistant_django/urls.py
+++ ta_assistant_django/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
-from classroom import views as class_views
 from django.conf import settings
 from django.conf.urls.static import static
 

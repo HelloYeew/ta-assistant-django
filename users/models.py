b201a7c605f709de5bf61d5e90feb2eca732b582
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-11 03:31:58 +0700

    Set user config model to bind with user

--- users/models.py
+++ users/models.py
@@ -89,4 +89,10 @@ class Profile(models.Model):
 
 # TODO: Make auto resize picture system
 
-# class UserConfig(models.Model):
+
+class UserConfig(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code_highlight = models.CharField(max_length=50, choices=CODE_HIGHLIGHT, default='atom-one-dark')
+
+    def __str__(self):
+        return f'{self.user.username} Config'
